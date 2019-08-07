660e4b74381f3ff667271af164197558519f1aa6
Thomas Fritzsche <tf@noto.de>
2019-08-07 21:15:44 +0200

    Refactor

--- morse.js
+++ morse.js
@@ -12,34 +12,16 @@ const keyShape = 0.003;
 const noSound = 0.000001;
 
 
-const code_map = {
-    a: '.-', b: '-...', c: '-.-.', d: '-..',
-    e: '.', f: '..-.', g: '--.', h: '....',
-    i: '..', j: '.---', k: '-.-', l: '.-..',
-    m: '--', n: '-.', o: '---', p: '.--.',
-    q: '--.-', r: '.-.', s: '...', t: '-',
-    u: '..-', v: '...-', w: '.--', x: '-..-',
-    y: '-.--', z: '--..', 1: '.----', 2: '..---',
-    3: '...--', 4: '....-', 5: '.....', 6: '-....',
-    7: '--...', 8: '---..', 9: '----.', 0: '-----',
 
-    '.': '.-.-.-', ',': '--..--', '?': '..--..',
-    "'": '.----.', '/': '-..-.', '(': '-.--.',
-    ')': '-.--.-', '&': '.-...', ':': '---...',
-    ';': '-.-.-.', '=': '-...-', '+': '.-.-.',
-    '-': '-....-', '_': '..--.-', '"': '.-..-.',
-    '$': '...-..-', '!': '-.-.--', '@': '.--.-.',
-    ' ': '/',
-};
 
-function tone(gainNode,t,l) {
+function tone(gainNode, t, l) {
     gainNode.gain.setValueAtTime(noSound, t)
     gainNode.gain.exponentialRampToValueAtTime(1, t + keyShape)
     t += l
 
     gainNode.gain.setValueAtTime(1, t);
     gainNode.gain.exponentialRampToValueAtTime(noSound, t + keyShape);
-    t += dit;   
+    t += dit;
     return t;
 }
 
@@ -56,26 +38,80 @@ function morse(code) {
     code.split("").forEach(function (letter) {
         switch (letter) {
             case ".":
-                t = tone(gainNode,t,dit);
+                t = tone(gainNode, t, dit);
                 break;
             case "-":
-                t = tone(gainNode,t,dah);
+                t = tone(gainNode, t, dah);
                 break;
             case " ":
-                t += 3 * dit;
+                t += dah
                 break;
         }
     });
 
     oscillator.connect(gainNode);
     gainNode.connect(ctx.destination);
-
     oscillator.start();
-
-    return false;
 }
 
+class Morse {
+    constructor() {
+        this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+        let t = this.ctx.currentTime;
+        this.gainNode = this.ctx.createGain();
+        // set audio to 
+        this.gainNode.gain.setValueAtTime(noSound, t);
+        this.oscillator = this.ctx.createOscillator()
+        this.oscillator.type = "sine";
+        this.oscillator.frequency.value = oscillatorFrequency;
+        this.oscillator.connect(this.gainNode);
+        this.gainNode.connect(this.ctx.destination);
+    }
+    toMorse(c) {
+        const code_map = {
+            a: '.-', b: '-...', c: '-.-.', d: '-..',
+            e: '.', f: '..-.', g: '--.', h: '....',
+            i: '..', j: '.---', k: '-.-', l: '.-..',
+            m: '--', n: '-.', o: '---', p: '.--.',
+            q: '--.-', r: '.-.', s: '...', t: '-',
+            u: '..-', v: '...-', w: '.--', x: '-..-',
+            y: '-.--', z: '--..', 1: '.----', 2: '..---',
+            3: '...--', 4: '....-', 5: '.....', 6: '-....',
+            7: '--...', 8: '---..', 9: '----.', 0: '-----',
+
+            '.': '.-.-.-', ',': '--..--', '?': '..--..',
+            "'": '.----.', '/': '-..-.', '(': '-.--.',
+            ')': '-.--.-', '&': '.-...', ':': '---...',
+            ';': '-.-.-.', '=': '-...-', '+': '.-.-.',
+            '-': '-....-', '_': '..--.-', '"': '.-..-.',
+            '$': '...-..-', '!': '-.-.--', '@': '.--.-.',
+            ' ': '/',
+        }
+        return code_map[i]
+    }
+    morse(code) {
+        let t = this.ctx.currentTime;
+        let that = this;
+        code.split("").forEach(function (letter) {
+            switch (letter) {
+                case ".":
+                    t = tone(that.gainNode, t, dit);
+                    break;
+                case "-":
+                    t = tone(that.gainNode, t, dah);
+                    break;
+                case " ":
+                    t += dah
+                    break;
+            }
+        });
+        this.oscillator.start();
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    morse("-.. .--- .---- - ..-.");
+ //   morse("-.. .--- .---- - ..-.");
+      let morse = new Morse()
+      morse.morse("-.. .--- .---- - ..-.")
+      
 });
